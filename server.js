6d18b37b540d74f4bd0a70718c7fa4ea94259c21
Mona Lisa <mekotigi@gmail.com>
2022-01-05 13:45:47 -0500

    add router commit

--- server.js
+++ server.js
@@ -1,24 +1,42 @@
+
 const express = require('express');
+const mysql = require('mysql2');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Express middleware
+// app.get('/', (req, res) => {
+//   res.json({
+//     message: 'Hello World'
+//   });
+// });
+
+
+
+// Express middleware or connection router
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({
-      message: 'Hello World'
-    });
-  });
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'mekotigi@gmail.com',
+    // Your MySQL password
+    password: 'hashHaset33*',
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
+);
 
-  // Default response for any other request (Not Found)
-app.use((req, res) => {
-    res.status(404).end();
-  });
 
-//connection function
+// Default response for any other request (Not Found)
+// app.use((req, res) => {
+//   res.status(404).end();
+// });
+
+
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-
+  console.log(`Server running on port ${PORT}`);
+});
